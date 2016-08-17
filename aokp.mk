201edbffa39456d12004290200f40a7b7c36d42e
Bule <1052239551@qq.com>
2016-08-17 20:10:26 +0800

    aokp

--- aokp.mk
+++ aokp.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aokp stuff.
+$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_kenzo
+PRODUCT_NAME := aokp_kenzo
 BOARD_VENDOR := xiaomi
 TARGET_VENDOR := xiaomi
 PRODUCT_DEVICE := kenzo
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_kenzo-userdebug
+add_lunch_combo aokp_kenzo-userdebug
